afdb7c792f18e7fca5629f2810e59590519104a4
sourabhyadav4664 <sourabhyadav1493@gmail.com>
2023-02-27 09:59:38 +0530

    done

--- contextclick.java
+++ contextclick.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.interactions.Actions;
 public class contextclick {
 	public static void main(String[] args) {
 		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.actitime.com/");
 		Actions action=new Actions(driver);
