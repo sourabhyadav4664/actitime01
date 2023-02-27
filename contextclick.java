a660b015d85b863c0f80b7a02ed2a4104369cc7d
sourabhyadav4664 <125717119+sourabhyadav4664@users.noreply.github.com>
2023-02-27 09:33:14 +0530

    Add files via upload

---
+++ contextclick.java
@@ -0,0 +1,19 @@
+package Action;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class contextclick {
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.actitime.com/");
+		Actions action=new Actions(driver);
+		action.contextClick().perform();
+	}
+
+}
